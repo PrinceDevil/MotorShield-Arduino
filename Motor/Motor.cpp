494b69c0b371175533f0f515d6eb4ea681ddeb17
PrinceDevil <princevergilius@gmail.com>
2019-01-14 20:57:32 +0800

    Add files via upload

---
+++ Motor/Motor.cpp
@@ -0,0 +1,39 @@
+#include"Motor.h"
+#include"Arduino.h"
+
+Motor::Motor(int arg1,int arg2,int arg3=-1){
+	in1=arg1;
+	pinMode(in1,OUTPUT);
+   /*
+      for L298P motor shield
+      L298P电机驱动
+    */
+    if (arg3==-1){
+      type=0;                      
+      en=arg2;
+    }
+    /*
+     * for L298N/L9110 motor shield
+     * L298N/L9110电机驱动
+     */
+    else{
+      type=1;
+      in2=arg2;
+      en=arg3;
+      pinMode(in2,OUTPUT);
+    }
+}
+void Motor::Forward(int v=255){
+  digitalWrite(in1,HIGH);
+  analogWrite(en,v);
+  if(type==1){digitalWrite(in2,LOW);}
+}
+void Motor::Backward(int v=255){
+  digitalWrite(in1,LOW);
+  analogWrite(en,v);
+  if(type==1){digitalWrite(in2,HIGH);}
+}
+void Motor::Stop(){
+  analogWrite(en,0);
+}
+
---
+++ Motor/Motor.h
@@ -0,0 +1,19 @@
+#ifndef Motor_h
+#define Motor_h
+
+#include "Arduino.h"
+
+class Motor{
+  public:
+	int in1;
+	int in2;
+	int en;
+	Motor(int arg1,int arg2,int arg3=-1);
+	void Forward(int v=255);
+	void Backward(int v=255);
+	void Stop();
+  
+  private:
+	int type;
+};
+#endif
